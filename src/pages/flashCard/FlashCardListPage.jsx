01bee3906ea871c78277b9f4f3ffe14815af5469
anuj sharma <as9737856@gmail.com>
2026-03-27 02:51:01 +0530

    file name fix

--- src/pages/flashCard/FlashCardListPage.jsx
+++ src/pages/flashCard/FlashCardListPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import flashcardService from "../../services/flashcardService.js";
+import flashcardService from "../../services/flashcardService";
 import PageHeader from "../../components/common/PageHeader.jsx";
 import Spinner from "../../components/common/Spinner.jsx";
 import FlashcardSetCard from "../../components/flashCards/FlashcardSetCard.jsx";
